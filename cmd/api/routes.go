148f999d842b4f1faa9918e2c27dbb8a07c1ba3c
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2023-08-21 00:45:01 +0300

    trimmed comments

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,8 +9,7 @@ func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
 	// Register the relevant methods, URL patterns and handler functions for our
-	// endpoints using the HandlerFunc() method. Note that http.MethodGet and
-	// http.MethodPost are constants which equate to the strings "GET" and "POST"
+	// endpoints using the HandlerFunc() method.
 	router.GET("/v1/healthcheck", app.healthcheckHandler)
 
 	router.POST("/v1/movies", app.createMovieHandler)
