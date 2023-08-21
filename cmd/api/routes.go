db27f9af105d7360364870ff9613e0903d94ebab
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2023-08-21 05:42:18 +0300

    Set custom error handlers for httprouter errors

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) routes() *httprouter.Router {
 	// Initialize a new router.
 	router := httprouter.New()
+
+	// Convert the error helpers to http.Handler(s) using the http.HandlerFunc() adapter,
+	// and then set them as the custom error handlers for 404 and 405 responses.
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	// Register the relevant methods, URL patterns and handler functions for our
 	// endpoints using the HandlerFunc() method.
