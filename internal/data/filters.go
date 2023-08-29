af2ef4ed92e1f29aefcbc0a4974e7677486e5251
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2023-08-29 08:31:08 +0300

    Validate query string parameters

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,7 +1,27 @@
 package data
 
+import (
+	"fmt"
+
+	"github.com/AustinMusiku/Greenlight/internal/validator"
+)
+
 type Filters struct {
-	Page     int
-	PageSize int
-	Sort     string
+	Page         int
+	PageSize     int
+	Sort         string
+	SortSafelist []string
+}
+
+func ValidateFilters(v *validator.Validator, f Filters) {
+	// Page parameters
+	v.Check(f.Page > 0, "page", "must be greater than zero")
+	v.Check(f.Page <= 10_000_000, "page", "must be a maximum of 10 million")
+
+	// Page_size parameters
+	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
+	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
+
+	// Check that the sort parameter matches a value in the safelist.
+	v.Check(validator.In(f.Sort, f.SortSafelist...), "sort", fmt.Sprintf("%v is not a valid sort parameter", f.Sort))
 }
