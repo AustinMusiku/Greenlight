827861e73bda4c436d2282ee02f80cc3e5062c45
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2023-09-05 06:54:28 +0300

    Authenticate users through a middleware

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -84,3 +84,11 @@ func (app *application) invalidCredentialsResponse(w http.ResponseWriter, r *htt
 	message := "invalid authentication credentials"
 	app.errorResponse(w, r, http.StatusUnauthorized, message)
 }
+
+// The invalidAuthenticationTokenResponse() method will be used to send a 401 Unauthorized
+// status code and JSON response to the client.
+func (app *application) invalidAuthenticationTokenResponse(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("WWW-Authenticate", "Bearer")
+	message := "invalid or missing authentication token"
+	app.errorResponse(w, r, http.StatusUnauthorized, message)
+}
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -36,5 +36,5 @@ func (app *application) routes() http.Handler {
 	// token routes
 	router.POST("/v1/tokens/authentication", app.createAuthenticationTokenHandler)
 
-	return app.recoverPanic(app.rateLimit(router))
+	return app.recoverPanic(app.rateLimit(app.authenticate(router)))
 }
